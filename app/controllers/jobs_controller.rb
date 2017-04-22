60e73bebcb5c48556adbc16b30aaf743aa12e807
wuzhao <liwuzhao97@gmail.com>
2017-04-22 17:36:21 +0800

    jobs/def index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,42 +3,25 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:quit]
 
 
-  # def index
-  #   if params[:category].present?
-  #     @category = params[:category]
-  #     if @category == 'all'
-  #       @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
-  #     else
-  #       @jobs = Job.where(:is_hidden => false, :category => @category).recent.paginate(:page => params[:page], :per_page => 10)
-  #     end
-  #
-  #   else
-  #     @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
-  #   end
-  # end
-
-
   def index
+    # 不分类
+    @jobs = Job.published
+
     # 按职位分类
     if params[:category].present?
       @category = params[:category]
-      if @category == '所有职位'
-        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 5)
-      else
-        @jobs = Job.published.where(:category => @category).recent.paginate(:page => params[:page], :per_page => 5)
-      end
-
-    # 不分类
-    else
-      @jobs = case params[:order]
-              when 'by_lower_bound'
-                Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
-              when 'by_upper_bound'
-                Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 5)
-              else
-                Job.published.recent.paginate(:page => params[:page], :per_page => 5)
-              end
+      @jobs = @jobs.where(:category => @category)
     end
+
+    #排序
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              @jobs.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
+            when 'by_upper_bound'
+              @jobs.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 5)
+            else
+              @jobs.published.recent.paginate(:page => params[:page], :per_page => 5)
+            end
 
   end
 
