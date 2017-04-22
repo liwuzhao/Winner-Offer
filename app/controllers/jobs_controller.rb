3973b3823502f2632b47372ac2e7f7c0a47dc80d
wuzhao <liwuzhao97@gmail.com>
2017-04-22 11:41:51 +0800

    category

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,17 +3,45 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:quit]
 
 
+  # def index
+  #   if params[:category].present?
+  #     @category = params[:category]
+  #     if @category == 'all'
+  #       @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+  #     else
+  #       @jobs = Job.where(:is_hidden => false, :category => @category).recent.paginate(:page => params[:page], :per_page => 10)
+  #     end
+  #
+  #   else
+  #     @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+  #   end
+  # end
+
+
   def index
-    @jobs = case params[:order]
-            when 'by_lower_bound'
-              Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 4)
-            when 'by_upper_bound'
-              Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 4)
-            else
-              Job.published.recent.paginate(:page => params[:page], :per_page => 4)
-            end
+    # 按职位分类
+    if params[:category].present?
+      @category = params[:category]
+      if @category == '所有职位'
+        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 5)
+      else
+        @jobs = Job.published.where(:category => @category).recent.paginate(:page => params[:page], :per_page => 5)
+      end
+
+    # 不分类
+    else
+      @jobs = case params[:order]
+              when 'by_lower_bound'
+                Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5)
+              when 'by_upper_bound'
+                Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 5)
+              else
+                Job.published.recent.paginate(:page => params[:page], :per_page => 5)
+              end
+    end
 
   end
+
 
   def show
     @job = Job.find(params[:id])
@@ -30,7 +58,7 @@ class JobsController < ApplicationController
     if !current_user
       redirect_to job_path(@job)
       flash[:warning] = "你需要先登录账号"
-    else if !current_user.is_member_of(@job)
+    elsif !current_user.is_member_of(@job)
       current_user.join!(@job)
       flash[:notice] = "收藏成功"
     else
@@ -38,7 +66,7 @@ class JobsController < ApplicationController
     end
       redirect_to job_path(@job)
     end
-end
+
 
 
   def quit
@@ -78,6 +106,4 @@ end
   def job_params
     params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden, :location, :company, :number, :category)
   end
-
-
 end
